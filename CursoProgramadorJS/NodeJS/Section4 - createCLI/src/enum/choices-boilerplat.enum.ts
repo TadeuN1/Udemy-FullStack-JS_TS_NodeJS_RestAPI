ac8c41dabe5effc7b01798e275e3341908f21c53
Tadeu Neres <tadeunrs@gmail.com>
2024-02-06 19:59:44 -0300

    format pc

--- CursoProgramadorJS/NodeJS/Section4 - createCLI/src/enum/choices-boilerplat.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum EChoicesBoilerPlate {
-  NODEJS_TS = 'NodeJs + TypeScript',
-    SCSS = '[Smacss] Arquitetura Scss'
-}
--- CursoProgramadorJS/NodeJS/Section4 - createCLI/src/enum/errors.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum EErrors {
-    ERROR_NULL = 'Insira um valor para o nome do projeto',
-    ERROR_SPECIAL_CHARACTERES = 'Não pode conter caracteres especiais',
-    ERROR_GIT_NAME = 'Não é possível criar pasta com esse nome.',
-    ERROR_INVALID_FOLDER = 'Nome de pasta já existente.'
-
-}
