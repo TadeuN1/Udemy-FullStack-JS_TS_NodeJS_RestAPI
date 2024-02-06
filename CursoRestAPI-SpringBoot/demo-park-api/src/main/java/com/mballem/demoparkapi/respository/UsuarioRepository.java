dbe8270d1e74d7413553c4552a2fd298072f9a21
Tadeu Neres <tadeunrs@gmail.com>
2024-02-06 20:00:51 -0300

    restart restAPi course

--- CursoRestAPI-SpringBoot/demo-park-api/src/main/java/com/mballem/demoparkapi/respository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mballem.demoparkapi.respository;
-
-import com.mballem.demoparkapi.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
--- CursoRestAPI-SpringBoot/demo-park-api/src/test/resources/sql/usuarios/usuarios-insert.sql
+++
@@ -1,3 +0,0 @@
-insert into USUARIOS(id, username, password, role) values (100, 'mae@email.com', '123456', 'ROLE_ADMIN');
-insert into USUARIOS(id, username, password, role) values (101, 'pai@email.com', '666666', 'ROLE_CLIENTE');
-insert into USUARIOS(id, username, password, role) values (102, 'irmao@email.com', '789456', 'ROLE_CLIENTE');
